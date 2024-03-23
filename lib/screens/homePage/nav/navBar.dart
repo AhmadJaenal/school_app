ab4cdd021f79a78cdc396c027693344ddbf5ee38
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-03-23 13:43:22 +0700

    [^] Update UI navBotton

--- lib/screens/homePage/nav/navBar.dart
+++ lib/screens/homePage/nav/navBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_app/screens/homePage/student/home_page.dart';
 import 'package:school_app/shared/theme.dart';
 
 class NavBarBottom extends StatefulWidget {
@@ -20,77 +21,102 @@ class _NavBarBottomState extends State<NavBarBottom> {
     });
   }
 
+  Widget body() {
+    switch (currentIndex) {
+      case 0:
+        return HomePage();
+      case 1:
+        return HomePage();
+      case 2:
+        return HomePage();
+      case 3:
+        return HomePage();
+      default:
+        return HomePage();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        padding: const EdgeInsets.all(0),
-        shape: const CircularNotchedRectangle(),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: AppColors.white,
-          elevation: 0,
-          currentIndex: currentIndex,
-          onTap: (value) {
-            setState(() {
-              currentIndex = value;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_presensi.png',
-                width: 24,
-                color: AppColors.darkBlue,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            border: Border(
+          top: BorderSide(
+            color: AppColors.black60,
+            width: 1.0,
+          ),
+        )),
+        child: BottomAppBar(
+          padding: const EdgeInsets.all(0),
+          shape: const CircularNotchedRectangle(),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: AppColors.white,
+            elevation: 0,
+            currentIndex: currentIndex,
+            onTap: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_presensi.png',
+                  width: 24,
+                  color: AppColors.darkBlue,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icon_presensi.png',
+                  width: 24,
+                ),
+                label: 'Presensi',
               ),
-              activeIcon: Image.asset(
-                'assets/icon_presensi.png',
-                width: 24,
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_edumart.png',
+                  width: 24,
+                  color: AppColors.darkBlue,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icon_edumart.png',
+                  width: 24,
+                  color: AppColors.primary1,
+                ),
+                label: 'EduMart',
               ),
-              label: 'Presensi',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_edumart.png',
-                width: 24,
-                color: AppColors.darkBlue,
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_edupay.png',
+                  width: 24,
+                  color: AppColors.darkBlue,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icon_edupay.png',
+                  width: 24,
+                  color: AppColors.primary1,
+                ),
+                label: 'EduPay',
               ),
-              activeIcon: Image.asset(
-                'assets/icon_edumart.png',
-                width: 24,
-                color: AppColors.primary1,
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_user.png',
+                  width: 24,
+                  color: AppColors.darkBlue,
+                ),
+                activeIcon: Image.asset(
+                  'assets/icon_user.png',
+                  width: 24,
+                  color: AppColors.primary1,
+                ),
+                label: 'Profil',
               ),
-              label: 'EduMart',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_edupay.png',
-                width: 24,
-                color: AppColors.darkBlue,
-              ),
-              activeIcon: Image.asset(
-                'assets/icon_edupay.png',
-                width: 24,
-                color: AppColors.primary1,
-              ),
-              label: 'EduPay',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_user.png',
-                width: 24,
-                color: AppColors.darkBlue,
-              ),
-              activeIcon: Image.asset(
-                'assets/icon_user.png',
-                width: 24,
-                color: AppColors.primary1,
-              ),
-              label: 'Profil',
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      body: body(),
     );
   }
 }
