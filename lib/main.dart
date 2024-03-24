d96c33a7ce0571ff0a96092ee51297ecd247c0de
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-03-24 13:14:21 +0700

    [+] Add Activity Absence

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
         ),
         GetPage(
           name: '/absence-history',
-          page: () => const AbsenceHistory(),
+          page: () => AbsenceHistory(),
         ),
       ],
     );
--- lib/screens/homePage/student/absence_history.dart
+++ lib/screens/homePage/student/absence_history.dart
@@ -1,18 +1,29 @@
+import 'package:intl/intl.dart';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:school_app/shared/theme.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class AbsenceHistory extends StatelessWidget {
-  const AbsenceHistory({super.key});
+  AbsenceHistory({super.key});
+
+  List<DateTime> selectedDays = [
+    DateTime(2024, 3, 12),
+    DateTime(2024, 3, 15),
+    DateTime(2024, 3, 17),
+  ];
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: AppColors.white,
       appBar: AppBar(
+        bottomOpacity: 0,
         elevation: 0,
         leading: GestureDetector(
           onTap: () => Get.back(),
@@ -65,30 +76,139 @@ class AbsenceHistory extends StatelessWidget {
               ],
             ),
             const Gap(16),
-            TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: DateTime.now(),
-              calendarStyle: CalendarStyle(
-                weekendTextStyle:
-                    AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
-              ),
-              daysOfWeekStyle: DaysOfWeekStyle(
-                weekdayStyle:
-                    AppTextStyle.paragraphM.copyWith(color: AppColors.black),
-                weekendStyle:
-                    AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
-              ),
-              headerStyle: HeaderStyle(
-                titleCentered: true,
-                formatButtonVisible: false,
-                titleTextStyle: AppTextStyle.paragraphMBold.copyWith(
+            SfDateRangePicker(
+              headerStyle: DateRangePickerHeaderStyle(
+                backgroundColor: AppColors.white,
+                textAlign: TextAlign.center,
+                textStyle: AppTextStyle.paragraphMBold.copyWith(
                   color: AppColors.black,
                 ),
               ),
-            )
+              selectionShape: DateRangePickerSelectionShape.circle,
+              selectionColor: AppColors.blue,
+              backgroundColor: AppColors.white,
+              selectionMode: DateRangePickerSelectionMode.multiRange,
+              initialSelectedDates: selectedDays,
+              showNavigationArrow: true,
+              monthCellStyle: DateRangePickerMonthCellStyle(
+                textStyle: AppTextStyle.paragraphMBold.copyWith(
+                  color: AppColors.black,
+                ),
+                weekendTextStyle: AppTextStyle.paragraphMBold.copyWith(
+                  color: AppColors.primary1,
+                ),
+              ),
+            ),
+
+            // TableCalendar(
+            //   firstDay: DateTime.utc(2010, 10, 16),
+            //   lastDay: DateTime.utc(2030, 3, 14),
+            //   focusedDay: DateTime.now(),
+            //   selectedDayPredicate: (day) {
+            //     return DateFormat.yMd().format(day) ==
+            //         DateFormat.yMd().format(_selectedDay);
+            //   },
+            //   calendarStyle: CalendarStyle(
+            //     weekendTextStyle:
+            //         AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
+            //     selectedDecoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(8),
+            //       color: Colors.blue,
+            //     ),
+            //   ),
+            //   daysOfWeekStyle: DaysOfWeekStyle(
+            //     weekdayStyle:
+            //         AppTextStyle.paragraphM.copyWith(color: AppColors.black),
+            //     weekendStyle:
+            //         AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
+            //   ),
+            //   headerStyle: HeaderStyle(
+            //     titleCentered: true,
+            //     formatButtonVisible: false,
+            //     titleTextStyle: AppTextStyle.paragraphMBold.copyWith(
+            //       color: AppColors.black,
+            //     ),
+            //   ),
+            // )
+            const Gap(19),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Aktivitas', style: AppTextStyle.paragraphLBold),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Lihat Semua',
+                    style: AppTextStyle.paragraphM
+                        .copyWith(color: AppColors.primary1),
+                  ),
+                )
+              ],
+            ),
+            const Gap(15),
+            CardActivity(),
+            CardActivity(),
+            CardActivity(),
+            CardActivity(),
+            const Gap(20),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding CardActivity() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Container(
+            width: 35,
+            height: 35,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: AppColors.info1.withOpacity(.1),
+            ),
+            child: Image.asset(
+              'assets/icon_login.png',
+            ),
+          ),
+          const Gap(16),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Masuk\n',
+                  style: AppTextStyle.paragraphLBold
+                      .copyWith(color: AppColors.black),
+                ),
+                TextSpan(
+                  text: '23 Agustus 2023',
+                  style: AppTextStyle.paragraphM
+                      .copyWith(color: AppColors.black80),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: '07:01\n',
+                  style: AppTextStyle.paragraphLBold
+                      .copyWith(color: AppColors.black),
+                ),
+                TextSpan(
+                  text: 'Tepat Waktu',
+                  style: AppTextStyle.paragraphM
+                      .copyWith(color: AppColors.black80),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
