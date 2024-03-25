e990600eae2642a19641f9fbb7181dc326a1b5c3
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-03-25 17:24:09 +0700

    [^] Update UI Edupay

--- lib/screens/homePage/edupay/edupay.dart
+++ lib/screens/homePage/edupay/edupay.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:school_app/shared/theme.dart';
+import 'package:school_app/widgets/custom_button.dart';
 
 class Edupay extends StatelessWidget {
   const Edupay({super.key});
@@ -33,11 +35,11 @@ class Edupay extends StatelessWidget {
                 ),
               ),
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         'Saldo EduPay',
@@ -102,6 +104,80 @@ class Edupay extends StatelessWidget {
                     ],
                   ),
                 ],
+              ),
+            ),
+            const Gap(17),
+            Container(
+              width: double.infinity,
+              height: 100,
+              padding: const EdgeInsets.fromLTRB(16, 20, 16, 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: AppColors.black40,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Tabungan',
+                        style: AppTextStyle.paragraphMBold
+                            .copyWith(color: AppColors.black100),
+                      ),
+                      Text(
+                        'Rp500.000',
+                        style:
+                            AppTextStyle.h2.copyWith(color: AppColors.black100),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Tarik Uang',
+                        style: AppTextStyle.paragraphXS
+                            .copyWith(color: AppColors.black100),
+                      ),
+                      SizedBox(
+                        width: 130,
+                        height: 32,
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            backgroundColor: AppColors.primary1,
+                            minimumSize: const Size(double.infinity, 56),
+                            padding: const EdgeInsets.symmetric(horizontal: 6),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          icon: Icon(Icons.file_upload_outlined,
+                              color: AppColors.white),
+                          label: Text(
+                            'Ambil Tabungan',
+                            style: AppTextStyle.paragraphSBold.copyWith(
+                              color: AppColors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Gap(17),
+            Text(
+              'Pembayaran Tagihan',
+              style: AppTextStyle.h3.copyWith(
+                color: AppColors.black,
               ),
             ),
           ],
