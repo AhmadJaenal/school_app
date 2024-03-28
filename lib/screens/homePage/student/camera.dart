992bedf1109a1cf25f9e806c1ef61d70a3e8d973
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-03-28 20:50:31 +0700

    [^] Update Dependenci

---
+++ lib/screens/homePage/student/camera.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
