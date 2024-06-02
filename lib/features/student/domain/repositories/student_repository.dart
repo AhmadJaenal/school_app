2434a8fdc45cb6cce8839e47ab7fca9d050c0a44
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-02 17:18:08 +0700

    [^] Update data Return

--- lib/features/student/domain/repositories/student_repository.dart
+++ lib/features/student/domain/repositories/student_repository.dart
@@ -1,8 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure.dart';
+import '../entities/activity.dart';
+import '../entities/event.dart';
 import '../entities/student.dart';
+import '../entities/transaction.dart';
 
 abstract class StudentRepository {
-  Future<StudentEntity> getStudentData(int id);
-  Future getHistoryTransaction();
-  Future getActivityStudent();
-  Future getEvent();
+  Future<Either<Failure, StudentEntity>> getStudentData(int id);
+  Future<Either<Failure, List<TransactionEntity>>> getHistoryTransaction();
+  Future<Either<Failure, List<ActivityStudentEntity>>> getActivityStudent();
+  Future<Either<Failure, List<EventEntity>>> getEvent();
 }
