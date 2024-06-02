326c67237fed472e084ecaa3a104938ee6f44826
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-02 10:42:30 +0700

    [+] Add button checkout

--- lib/screens/homePage/edumart/cart_product.dart
+++ lib/screens/homePage/edumart/cart_product.dart
@@ -1,11 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:school_app/shared/theme.dart';
 import 'package:school_app/widgets/card_cart_product.dart';
+import 'package:school_app/widgets/custom_button.dart';
 
-class CartProduct extends StatelessWidget {
+class CartProduct extends StatefulWidget {
   const CartProduct({super.key});
 
+  @override
+  State<CartProduct> createState() => _CartProductState();
+}
+
+bool _switchValue = true;
+
+class _CartProductState extends State<CartProduct> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +41,80 @@ class CartProduct extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 7,
-        itemBuilder: (context, index) => cartProduct(),
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 150),
+        child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: 7,
+          itemBuilder: (context, index) => cartProduct(),
+        ),
       ),
+      floatingActionButton: Container(
+        width: double.infinity,
+        height: 140,
+        color: AppColors.white,
+        padding: EdgeInsets.symmetric(
+          horizontal: AppMargin.defaultMargin,
+          vertical: 12,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/icon_coin.png',
+                  width: 24,
+                  color: AppColors.warning2,
+                ),
+                const Gap(4),
+                Text(
+                  "Tukarkan 1000 poin",
+                  style: AppTextStyle.paragraphL
+                      .copyWith(color: AppColors.black100),
+                ),
+                const Gap(4),
+                Icon(Icons.help_outline_rounded, color: AppColors.black60),
+                const Spacer(),
+                Switch(
+                  activeColor: AppColors.white,
+                  activeTrackColor: AppColors.primary1,
+                  value: _switchValue,
+                  onChanged: (value) {
+                    setState(() {
+                      _switchValue = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    style: AppTextStyle.paragraphM
+                        .copyWith(color: AppColors.black),
+                    children: <TextSpan>[
+                      const TextSpan(
+                        text: 'Total\n',
+                      ),
+                      TextSpan(
+                        text: 'Rp72.000',
+                        style: AppTextStyle.h3.copyWith(color: AppColors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 130,
+                  child: PrimaryButton(titleButton: 'Checkout', ontap: () {}),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
