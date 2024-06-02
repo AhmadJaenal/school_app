d3a4ad0ce159333cd823ea78cf10498ccdb5ed7d
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-02 16:43:25 +0700

    [-] Delete lib project

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:school_app/screens/authentication/forgot_password.dart';
-import 'package:school_app/screens/authentication/login.dart';
-import 'package:school_app/screens/authentication/register.dart';
-import 'package:school_app/screens/homePage/edumart/cart_product.dart';
-import 'package:school_app/screens/homePage/edumart/detail_product.dart';
-import 'package:school_app/screens/homePage/edumart/edumart.dart';
-import 'package:school_app/screens/homePage/edumart/history_edumart.dart';
-import 'package:school_app/screens/homePage/edupay/bill_page.dart';
-import 'package:school_app/screens/homePage/edupay/edupay.dart';
-import 'package:school_app/screens/homePage/edupay/invoice_spp.dart';
-import 'package:school_app/screens/homePage/edupay/payment_spp.dart';
-import 'package:school_app/screens/homePage/event/list_event.dart';
-import 'package:school_app/screens/homePage/nav/nav_bar.dart';
-import 'package:school_app/screens/homePage/notification/notification.dart';
-import 'package:school_app/screens/homePage/parent/add_children.dart';
-import 'package:school_app/screens/homePage/parent/list_children.dart';
-import 'package:school_app/screens/homePage/profile/change_password.dart';
-import 'package:school_app/screens/homePage/profile/detail_profile.dart';
-import 'package:school_app/screens/homePage/profile/edit_profile.dart';
-import 'package:school_app/screens/homePage/profile/history_payment.dart';
-import 'package:school_app/screens/homePage/profile/profile_page.dart';
-import 'package:school_app/screens/homePage/student/absence_history.dart';
-import 'package:school_app/screens/homePage/student/activity.dart';
-import 'package:school_app/screens/homePage/student/camera.dart';
-import 'package:school_app/screens/homePage/student/home_page.dart';
-import 'package:school_app/screens/homePage/student/permit_application.dart';
-import 'package:school_app/screens/onBoarding/on_boarding.dart';
-
-import 'screens/homePage/parent/detail_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,119 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const OnBoarding(),
-      getPages: [
-        GetPage(
-          name: '/login',
-          page: () => const Login(),
+      home: Scaffold(
+        body: Center(
+          child: Text("Test"),
         ),
-        GetPage(
-          name: '/register',
-          page: () => Register(),
-        ),
-        GetPage(
-          name: '/forgot-password',
-          page: () => const ForgotPassword(),
-        ),
-        GetPage(
-          name: '/home-page',
-          page: () => const HomePage(),
-        ),
-        GetPage(
-          name: '/nav',
-          page: () => const NavBarBottom(),
-        ),
-        GetPage(
-          name: '/list-event',
-          page: () => ListEvent(),
-        ),
-        GetPage(
-          name: '/absence-history',
-          page: () => AbsenceHistory(),
-        ),
-        GetPage(
-          name: '/permit-application',
-          page: () => PermitApplication(),
-        ),
-        GetPage(
-          name: '/notification',
-          page: () => NotificationPage(),
-        ),
-        GetPage(
-          name: '/activity-student',
-          page: () => ActivityStudent(),
-        ),
-        GetPage(
-          name: '/add-children',
-          page: () => AddChildren(),
-        ),
-        GetPage(
-          name: '/list-children',
-          page: () => const ListChildren(),
-        ),
-        GetPage(
-          name: '/detail-data-children',
-          page: () => const DetailData(),
-        ),
-        GetPage(
-          name: '/edupay',
-          page: () => Edupay(),
-        ),
-        GetPage(
-          name: '/camera',
-          page: () => const CameraPage(),
-        ),
-        GetPage(
-          name: '/profile-page',
-          page: () => const ProfilePage(),
-        ),
-        GetPage(
-          name: '/detail-profile',
-          page: () => const DetailProfile(),
-        ),
-        GetPage(
-          name: '/change-password',
-          page: () => const ChangePassword(),
-        ),
-        GetPage(
-          name: '/edit-profile',
-          page: () => const EditProfile(),
-        ),
-        GetPage(
-          name: '/history-payment',
-          page: () => const HistoryPayment(),
-        ),
-        GetPage(
-          name: '/bill-page',
-          page: () => const BillPage(),
-        ),
-        GetPage(
-          name: '/payment-spp',
-          page: () => const PaymentSPP(),
-        ),
-        GetPage(
-          name: '/invoice',
-          page: () => const InvoiceSPP(),
-        ),
-        GetPage(
-          name: '/edumart',
-          page: () => const EdumartPage(),
-        ),
-        GetPage(
-          name: '/detail-product',
-          page: () => const DetailProduct(),
-        ),
-        GetPage(
-          name: '/cart-product',
-          page: () => const CartProduct(),
-        ),
-        GetPage(
-          name: '/history-edumart',
-          page: () => const HistoryEdumart(),
-        ),
-      ],
+      ),
     );
   }
 }
