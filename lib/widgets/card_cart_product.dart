f9e4385e95b710204c00e61249c295127ba6c5e0
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-02 10:17:23 +0700

    Fix [^] Update widget cart product

--- lib/widgets/card_cart_product.dart
+++ lib/widgets/card_cart_product.dart
@@ -3,121 +3,115 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:school_app/shared/theme.dart';
 
-class CartProduct extends StatelessWidget {
-  const CartProduct({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 180,
-      padding: EdgeInsets.symmetric(
-        horizontal: AppMargin.defaultMargin,
-        vertical: 16,
-      ),
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: AppColors.black60,
-            width: 1,
-          ),
+Widget cartProduct() {
+  return Container(
+    width: double.infinity,
+    padding: EdgeInsets.symmetric(
+      horizontal: AppMargin.defaultMargin,
+      vertical: 16,
+    ),
+    decoration: BoxDecoration(
+      border: Border(
+        bottom: BorderSide(
+          color: AppColors.black60,
+          width: 1,
         ),
       ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(CupertinoIcons.bag, color: AppColors.primary1),
-              const Gap(6),
-              Text(
-                'EduMart 1',
-                style: AppTextStyle.paragraphLBold
-                    .copyWith(color: AppColors.black100),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Icon(CupertinoIcons.bag, color: AppColors.primary1),
+            const Gap(6),
+            Text(
+              'EduMart 1',
+              style: AppTextStyle.paragraphLBold
+                  .copyWith(color: AppColors.black100),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.keyboard_arrow_right_rounded,
+                color: AppColors.black60,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.keyboard_arrow_right_rounded,
-                  color: AppColors.black60,
-                ),
-              )
-            ],
-          ),
-          const Gap(12),
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset(
-                  'assets/image_product.png',
-                  width: 72,
-                  height: 72,
-                  fit: BoxFit.cover,
-                ),
+            )
+          ],
+        ),
+        const Gap(12),
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                'assets/image_product.png',
+                width: 72,
+                height: 72,
+                fit: BoxFit.cover,
               ),
-              const Gap(16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            const Gap(16),
+            RichText(
+              text: TextSpan(
+                style: AppTextStyle.paragraphL.copyWith(color: AppColors.black),
                 children: [
-                  Text(
-                    'Kopi Gojo',
-                    style: AppTextStyle.paragraphLBold
-                        .copyWith(color: AppColors.black100),
+                  const TextSpan(
+                    text: 'Kopi Gojo\n',
                   ),
-                  const Gap(4),
-                  Text(
-                    'Makanan',
+                  TextSpan(
+                    text: 'Makanan\n',
                     style: AppTextStyle.paragraphS
-                        .copyWith(color: AppColors.black80),
+                        .copyWith(color: AppColors.black),
                   ),
-                  const Gap(2),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      border: Border.all(color: AppColors.black80, width: 1),
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(CupertinoIcons.minus,
-                            color: AppColors.black80, size: 16),
-                        const Gap(4),
-                        Text(
-                          '1',
-                          style: AppTextStyle.paragraphM.copyWith(
-                            color: AppColors.black100,
+                  WidgetSpan(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        border: Border.all(color: AppColors.black80, width: 1),
+                      ),
+                      margin: const EdgeInsets.only(top: 5),
+                      child: Row(
+                        children: [
+                          Icon(CupertinoIcons.minus,
+                              color: AppColors.black80, size: 16),
+                          const Gap(8),
+                          Text(
+                            '1',
+                            style: AppTextStyle.paragraphM.copyWith(
+                              color: AppColors.black100,
+                            ),
                           ),
-                        ),
-                        const Gap(4),
-                        Icon(Icons.add, color: AppColors.black80, size: 16),
-                      ],
+                          const Gap(8),
+                          Icon(Icons.add, color: AppColors.black80, size: 16),
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
-              const Spacer(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.close,
-                    color: AppColors.black100,
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                Icon(
+                  Icons.close,
+                  color: AppColors.black100,
+                ),
+                const Gap(24),
+                Text(
+                  'Rp24.000',
+                  style: AppTextStyle.paragraphSBold.copyWith(
+                    color: AppColors.secondary1,
                   ),
-                  const Gap(24),
-                  Text(
-                    'Rp24.000',
-                    style: AppTextStyle.paragraphSBold.copyWith(
-                      color: AppColors.secondary1,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
