59db9613ab88706c5a765b814d780033ef5344df
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-02 10:17:52 +0700

    [-] Delete unused code

--- lib/screens/homePage/edumart/cart_product.dart
+++ lib/screens/homePage/edumart/cart_product.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:school_app/shared/theme.dart';
 import 'package:school_app/widgets/card_cart_product.dart';
