9e35d4bc4eb92fc0b997bf9683f39a71cf39a787
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-05 21:09:48 +0700

    [+] Add remote datasource

---
+++ lib/features/student/data/datasources/remote_datasource.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import '../../domain/entities/activity.dart';
+import '../../domain/entities/event.dart';
+import '../../domain/entities/student.dart';
+import '../../domain/entities/transaction.dart';
+
+abstract class StudentRemoteDatasource {
+  Future<StudentEntity> getStudentData(String id);
+  Future<List<TransactionEntity>> getHistoryTransaction();
+  Future<List<ActivityStudentEntity>> getActivityStudent();
+  Future<List<EventEntity>> getEvent();
+}
+
+class StudentRemoteDatasourceImplementation extends StudentRemoteDatasource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<StudentEntity> getStudentData(String id) async {
+    DocumentSnapshot doc = await _firestore.collection("student").doc(id).get();
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ActivityStudentEntity>> getActivityStudent() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<EventEntity>> getEvent() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<TransactionEntity>> getHistoryTransaction() {
+    throw UnimplementedError();
+  }
+}
