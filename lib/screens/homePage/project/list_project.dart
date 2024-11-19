2100360236b958871bada75bc164b19bf5a7e8b9
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-19 09:33:36 +0700

    feat: add project management features (create, delete, retrieve) and project service

--- lib/screens/homePage/project/list_project.dart
+++ lib/screens/homePage/project/list_project.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:school_app/models/project.dart';
-import 'package:school_app/services/project/project.dart';
-import 'package:school_app/shared/theme.dart';
+import 'package:school_app/core/platform/status.dart';
+import 'package:school_app/datasource/local_datasource.dart';
+import 'package:school_app/models/user.dart';
+import 'package:school_app/widgets/custom_button.dart';
+import 'package:school_app/widgets/custom_popup_message.dart';
+import '../../../models/project.dart';
+import '../../../services/project/project.dart';
+import '../../../shared/theme.dart';
 
-import 'package:school_app/widgets/card_project.dart';
+import '../../../widgets/card_project.dart';
 
 class ListProject extends StatelessWidget {
   const ListProject({super.key});
@@ -13,6 +19,22 @@ class ListProject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ProjectProvider projectProvider = Provider.of<ProjectProvider>(context);
+    UserPreferences userPrefs = UserPreferences();
+
+    var loading = const Center(child: CircularProgressIndicator());
+
+    deleteProject() async {
+      final Future<Map<String, dynamic>> successfulMessage =
+          projectProvider.deleteProjectById();
+
+      successfulMessage.then((response) {
+        if (response['status']) {
+          popUpSubmission(context, true);
+        } else {
+          popUpSubmission(context, false);
+        }
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -33,26 +55,117 @@ class ListProject extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.fromLTRB(24, 14, 24, 16),
         child: FutureBuilder(
-          future: projectProvider.getAllProject(),
+          future: userPrefs.getUser(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else {
-              if (snapshot.hasData) {
-                List<Project> project = snapshot.data!['data'];
-                return ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: project.length,
-                  itemBuilder: (context, index) =>
-                      CardProject(project: project[index]),
-                );
-              } else {
-                return const Text('Belum ada project yang bisa dikerjakan');
-              }
+            if (snapshot.hasData) {
+              User userData = snapshot.data!;
+              return FutureBuilder(
+                future: userData.roles!.contains('intern')
+                    ? projectProvider.getProjectByAssigment()
+                    : projectProvider.getAllProject(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData) {
+                    return Center(
+                        child: Text(
+                      'Belum ada project yang bisa ditugaskan',
+                      style: AppTextStyle.paragraphL,
+                    ));
+                  }
+                  List<Project> project = snapshot.data!['data'];
+                  return ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: project.length,
+                    itemBuilder: (context, index) => CardProject(
+                        project: project[index],
+                        onLongPress: () => userData.roles!.contains('staff')
+                            ? showModalBottomSheet(
+                                context: context,
+                                backgroundColor: Colors.transparent,
+                                elevation: 0,
+                                builder: (context) => Container(
+                                  width: double.infinity,
+                                  height: 185,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 32, vertical: 22),
+                                  margin: EdgeInsets.symmetric(
+                                    horizontal: AppMargin.defaultMargin,
+                                    vertical: 28,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.white,
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Hapus Proyek',
+                                        style: AppTextStyle.h3.copyWith(
+                                          color: AppColors.darkBlue,
+                                        ),
+                                      ),
+                                      const Gap(8),
+                                      Text(
+                                        'Apakah Anda yakin ingin hapus proyek ini?',
+                                        style: AppTextStyle.paragraphL.copyWith(
+                                          color: AppColors.darkBlue,
+                                        ),
+                                      ),
+                                      const Gap(8),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          SizedBox(
+                                            width: 130,
+                                            child: SecondaryButton(
+                                                titleButton: 'Batal',
+                                                ontap: () => Get.back()),
+                                          ),
+                                          projectProvider.projectStatus ==
+                                                  ProcessState.delete
+                                              ? loading
+                                              : SizedBox(
+                                                  width: 130,
+                                                  child: PrimaryButton(
+                                                    titleButton: 'Ya',
+                                                    ontap: () {
+                                                      deleteProject();
+                                                      Get.back();
+                                                    },
+                                                  ),
+                                                ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : {}),
+                  );
+                },
+              );
             }
+            return const SizedBox();
           },
         ),
       ),
+    );
+  }
+
+  Future<dynamic> popUpSubmission(BuildContext context, bool isSuccess) {
+    return showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      builder: (context) => PopUpMessage(
+          isSuccess: isSuccess,
+          successMessage: 'Proyek berhasil dihapus!',
+          failedMessage: 'Proyek gagal dihapus!'),
     );
   }
 }
--- lib/services/project/project.dart
+++ lib/services/project/project.dart
@@ -1,3 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:school_app/core/platform/status.dart';
+
+import '../../api/urls.dart';
+import '../../models/project.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dart:developer' as dev;
+
+class ProjectProvider with ChangeNotifier {
+  ProcessState _projectStatus = ProcessState.idle;
+  ProcessState get projectStatus => _projectStatus;
+
+  Future<Map<String, dynamic>> getAllProject() async {
+    Map<String, dynamic> result;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessToken')!;
+
+    Response response = await get(Uri.parse(URLs.allProject), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $accessToken'
+    });
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      var projectData = responseData['data'];
+
+      List<Project> listProject = Project.fromJsonList(projectData);
+      result = {'status': true, 'message': 'Successful', 'data': listProject};
+    } else {
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['error']
+      };
+    }
+    return result;
+  }
+
   Future<Map<String, dynamic>> getAllProjectById() async {
     Map<String, dynamic> result;
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -24,3 +66,69 @@
     }
     return result;
   }
+
+  Future<Map<String, dynamic>> getProjectByAssigment() async {
+    Map<String, dynamic> result;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessToken')!;
+
+    Response response = await get(
+      Uri.parse(URLs.getProjectByAssignment),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+    dev.log(response.statusCode.toString(), name: 'response code project');
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      var projectData = responseData['data']['projects'];
+
+      List<Project> project = Project.fromJsonList(projectData);
+      result = {'status': true, 'message': 'Successful', 'data': project};
+    } else {
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['error']
+      };
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> deleteProjectById() async {
+    Map<String, dynamic> result;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessToken')!;
+    int projectId = prefs.getInt('projectId')!;
+
+    Response response = await delete(
+      Uri.parse('${URLs.deleteProjectById}$projectId/delete'),
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+    dev.log(response.body.toString(), name: 'response code project');
+
+    _projectStatus = ProcessState.uploading;
+    notifyListeners();
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      var projectData = responseData['data'];
+
+      _projectStatus = ProcessState.success;
+      notifyListeners();
+
+      result = {'status': true, 'message': 'Successful'};
+    } else {
+      _projectStatus = ProcessState.failure;
+      notifyListeners();
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['errors']
+      };
+    }
+    return result;
+  }
+}
