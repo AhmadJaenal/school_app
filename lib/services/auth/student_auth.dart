9cbd23fba3ef759249d2d2131c227d38492b28bd
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-19 09:21:48 +0700

    feat: add feature to add student data

--- lib/services/auth/student_auth.dart
+++ lib/services/auth/student_auth.dart
@@ -49,12 +49,38 @@ class StudentAuthProvider with ChangeNotifier {
       User authUser = User.fromJson(userData);
       UserPreferences().saveUser(authUser);
 
-      _loggedInStatus = Status.loggedIn;
+  Future<Map<String, dynamic>> register(
+      {required String name, email, password, required int schoolId}) async {
+    Map<String, dynamic> result;
+
+    final Map<String, dynamic> loginData = {
+      'name': name,
+      'school_id': schoolId,
+      'email': email,
+      'password': password,
+    };
+
+    _registerInStatus = AuthStatus.registering;
+    notifyListeners();
+
+    Response response = await post(
+      Uri.parse(URLs.register),
+      body: json.encode(loginData),
+      headers: {'Content-Type': 'application/json'},
+    );
+    dev.log(name: "respone code register", response.statusCode.toString());
+
+    if (response.statusCode == 201) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      var userData = responseData['data'];
+
+      User authUser = User.fromJson(userData);
+      _registerInStatus = AuthStatus.registered;
       notifyListeners();
 
       result = {'status': true, 'message': 'Successful', 'data': authUser};
     } else {
-      _loggedInStatus = Status.notLoggedIn;
+      _registerInStatus = AuthStatus.notRegistered;
       notifyListeners();
       result = {
         'status': false,
