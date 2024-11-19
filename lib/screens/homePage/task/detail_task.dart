abecdaa8c516ed5c48674d142ce16f5d57121dd2
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-19 09:29:53 +0700

    feat: add feature to view task details

--- lib/screens/homePage/task/detail_task.dart
+++ lib/screens/homePage/task/detail_task.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:school_app/models/Task.dart';
+import 'package:school_app/datasource/local_datasource.dart';
+import 'package:school_app/models/task.dart';
 import 'package:school_app/services/task/task_service.dart';
 import 'package:school_app/widgets/custom_button.dart';
 
@@ -14,8 +15,7 @@ class DetailTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TaskProvider taskProvider = Provider.of<TaskProvider>(context);
-
-    var task = Get.arguments;
+    UserPreferences userPrefs = UserPreferences();
 
     return Scaffold(
       appBar: AppBar(
@@ -33,40 +33,118 @@ class DetailTask extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: FutureBuilder(
-        future: taskProvider.getTaskByTaskId(task['id']),
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 170),
+        child: ListView(
+          children: [
+            FutureBuilder(
+              future: taskProvider.getTaskByTaskId(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+                if (!snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Center(
+                      child: Text('Detail tugas tidak tersedia',
+                          style: AppTextStyle.paragraphL),
+                    ),
+                  );
+                }
+                TaskModel task = snapshot.data!['data'];
+                return Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: AppMargin.defaultMargin),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(task.title!, style: AppTextStyle.h3),
+                      const Gap(8),
+                      Text(task.desc!, style: AppTextStyle.paragraphL),
+                    ],
+                  ),
+                );
+              },
+            ),
+            const Gap(16),
+            FutureBuilder(
+              future: userPrefs.getUser(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.data!.roles!.contains('intern')) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: AppMargin.defaultMargin),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Submission', style: AppTextStyle.h3),
+                        const Gap(8),
+                        SizedBox(
+                          width: 130,
+                          height: 32,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.toNamed('/submission-task');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: AppColors.primary1,
+                              minimumSize: const Size(double.infinity, 56),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 6),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              'Tugas Kamu',
+                              style: AppTextStyle.paragraphSBold.copyWith(
+                                color: AppColors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FutureBuilder(
+        future: userPrefs.getUser(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+          if (snapshot.hasData && snapshot.data!.roles!.contains('intern')) {
+            return Padding(
+              padding: EdgeInsets.all(AppMargin.defaultMargin),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  PrimaryButton(
+                    ontap: () {},
+                    titleButton: 'Module',
+                  ),
+                  const Gap(10),
+                  PrimaryButton(
+                    ontap: () => Get.toNamed('/add-submission'),
+                    titleButton: 'Add Submission',
+                  ),
+                ],
+              ),
+            );
           } else {
-            if (snapshot.hasData) {
-              TaskModel task = snapshot.data!['data'];
-              return Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: AppMargin.defaultMargin),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(task.title!, style: AppTextStyle.paragraphLBold),
-                    const Gap(8),
-                    Text(task.desc!, style: AppTextStyle.paragraphL),
-                  ],
-                ),
-              );
-            } else {
-              return const Center(
-                child: Text('Detail tugas tidak tersedia'),
-              );
-            }
+            return const SizedBox();
           }
         },
-      ),
-      floatingActionButton: Padding(
-        padding: EdgeInsets.all(AppMargin.defaultMargin),
-        child: PrimaryButton(
-          ontap: () {},
-          titleButton: 'Module',
-        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
