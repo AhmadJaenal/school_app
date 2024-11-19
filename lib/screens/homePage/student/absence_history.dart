a862cda11ec51c353e4111e1b18c511d8475743c
Ahmad Jaenal <ahmadjaenal629@gmail.com>
2024-11-19 09:45:31 +0700

    fix: implement attendance history feature

--- lib/screens/homePage/student/absence_history.dart
+++ lib/screens/homePage/student/absence_history.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
-import 'package:school_app/datasource/local_datasource.dart';
-import 'package:school_app/models/presence.dart';
-import 'package:school_app/services/presence_service.dart';
-import 'package:school_app/shared/theme.dart';
-import 'package:school_app/widgets/card_absence.dart';
-import 'package:school_app/widgets/card_activity.dart';
-import 'package:school_app/widgets/custom_button.dart';
-// import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import '../../../datasource/local_datasource.dart';
+import '../../../models/presence.dart';
+import '../../../services/presence_service.dart';
+import '../../../shared/theme.dart';
+import '../../../widgets/card_absence.dart';
+import '../../../widgets/card_activity.dart';
 
 // ignore: must_be_immutable
 class AbsenceHistory extends StatelessWidget {
-  AbsenceHistory({super.key});
-
-  List<DateTime> selectedDays = [
-    DateTime(2024, 3, 12),
-    DateTime(2024, 3, 15),
-    DateTime(2024, 3, 17),
-  ];
+  const AbsenceHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    // double height = MediaQuery.of(context).size.height;
 
     PresenceProvider presenceProvider = Provider.of<PresenceProvider>(context);
+    UserPreferences userPrefs = UserPreferences();
 
     return Scaffold(
       backgroundColor: AppColors.white,
@@ -51,53 +45,53 @@ class AbsenceHistory extends StatelessWidget {
             FutureBuilder(
               future: UserPreferences().getPresence(),
               builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData) {
-                    CountPresence countPresence = snapshot.data!;
-                    return Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CardAbsence(
-                              width: width,
-                              title: 'Jumlah Hadir',
-                              color: AppColors.green,
-                              amount: countPresence.present!,
-                            ),
-                            CardAbsence(
-                              width: width,
-                              title: 'Jumlah Izin',
-                              color: AppColors.info1,
-                              amount: countPresence.permission!,
-                            ),
-                          ],
-                        ),
-                        const Gap(14),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CardAbsence(
-                              width: width,
-                              title: 'Jumlah Sakit',
-                              color: AppColors.warning1,
-                              amount: countPresence.sick!,
-                            ),
-                            CardAbsence(
-                              width: width,
-                              title: 'Jumlah Alpa',
-                              color: AppColors.danger1,
-                              amount: countPresence.absent ?? 0,
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  } else {
-                    return Text('Loading');
-                  }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasData) {
+                  CountPresence countPresence = snapshot.data!;
+                  return Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CardAbsence(
+                            width: width,
+                            title: 'Jumlah Hadir',
+                            color: AppColors.green,
+                            amount: countPresence.present ?? 0,
+                          ),
+                          CardAbsence(
+                            width: width,
+                            title: 'Jumlah Izin',
+                            color: AppColors.info1,
+                            amount: countPresence.permission ?? 0,
+                          ),
+                        ],
+                      ),
+                      const Gap(14),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CardAbsence(
+                            width: width,
+                            title: 'Jumlah Sakit',
+                            color: AppColors.warning1,
+                            amount: countPresence.sick ?? 0,
+                          ),
+                          CardAbsence(
+                            width: width,
+                            title: 'Jumlah Alpa',
+                            color: AppColors.danger1,
+                            amount: countPresence.absent ?? 0,
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
                 } else {
-                  return const SizedBox();
+                  return const Text('Loading');
                 }
               },
             ),
@@ -114,154 +108,61 @@ class AbsenceHistory extends StatelessWidget {
               child: FutureBuilder(
                 future: presenceProvider.getPresenceByUserId(),
                 builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      List<Presence> presence = snapshot.data!['data'];
-                      return GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 10,
-                          crossAxisSpacing: 5.0,
-                          mainAxisSpacing: 5.0,
-                          childAspectRatio: 1,
-                        ),
-                        itemCount: presence.length,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                backgroundColor: Colors.transparent,
-                                elevation: 0,
-                                builder: (context) => Container(
-                                  width: double.infinity,
-                                  height: 165,
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 32, vertical: 22),
-                                  margin: EdgeInsets.symmetric(
-                                    horizontal: AppMargin.defaultMargin,
-                                    vertical: 28,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: AppColors.white,
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Hari Ke-${index + 1} Anda Magang',
-                                        style: AppTextStyle.h3.copyWith(
-                                          color: AppColors.darkBlue,
-                                        ),
-                                      ),
-                                      const Gap(4),
-                                      Text(
-                                        'Absensi: ${presence[index].status}',
-                                        style: AppTextStyle.paragraphL.copyWith(
-                                          color: AppColors.darkBlue,
-                                        ),
-                                      ),
-                                      const Gap(8),
-                                      PrimaryButton(
-                                        titleButton: 'Tugas yang dikerjakan',
-                                        ontap: () {},
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              width: 10,
-                              height: 10,
-                              decoration: BoxDecoration(
-                                color: presence[index].status == 'Hadir'
-                                    ? AppColors.green
-                                    : presence[index].status == 'Sakit'
-                                        ? AppColors.warning2
-                                        : presence[index].status == 'Izin'
-                                            ? AppColors.info1
-                                            : AppColors.danger2,
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '${index + 1}',
-                                  style: AppTextStyle.paragraphS
-                                      .copyWith(color: AppColors.white),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    } else {
-                      return const Center(child: Text('Terjadi Kesalahan'));
-                    }
-                  } else {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Center(child: CircularProgressIndicator());
                   }
+
+                  if (snapshot.data!['status']) {
+                    List<Presence> listPresence = snapshot.data!['data'];
+                    return UserAbsenceGrid(listPresence: listPresence);
+                  }
+                  return Center(
+                      child: Text('Siswa belum pernah melakukan absensi',
+                          style: AppTextStyle.paragraphM
+                              .copyWith(color: AppColors.black80)));
                 },
               ),
             ),
-
-            // SfDateRangePicker(
-            //   headerStyle: DateRangePickerHeaderStyle(
-            //     backgroundColor: AppColors.white,
-            //     textAlign: TextAlign.center,
-            //     textStyle: AppTextStyle.paragraphMBold.copyWith(
-            //       color: AppColors.black,
-            //     ),
-            //   ),
-            //   selectionShape: DateRangePickerSelectionShape.circle,
-            //   selectionColor: AppColors.blue,
-            //   backgroundColor: AppColors.white,
-            //   selectionMode: DateRangePickerSelectionMode.multiRange,
-            //   initialSelectedDates: selectedDays,
-            //   showNavigationArrow: true,
-            //   monthCellStyle: DateRangePickerMonthCellStyle(
-            //     textStyle: AppTextStyle.paragraphMBold.copyWith(
-            //       color: AppColors.black,
-            //     ),
-            //     weekendTextStyle: AppTextStyle.paragraphMBold.copyWith(
-            //       color: AppColors.primary1,
-            //     ),
-            //   ),
-            // ),
-
-            // TableCalendar(
-            //   firstDay: DateTime.utc(2010, 10, 16),
-            //   lastDay: DateTime.utc(2030, 3, 14),
-            //   focusedDay: DateTime.now(),
-            //   selectedDayPredicate: (day) {
-            //     return DateFormat.yMd().format(day) ==
-            //         DateFormat.yMd().format(_selectedDay);
-            //   },
-            //   calendarStyle: CalendarStyle(
-            //     weekendTextStyle:
-            //         AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
-            //     selectedDecoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(8),
-            //       color: Colors.blue,
-            //     ),
-            //   ),
-            //   daysOfWeekStyle: DaysOfWeekStyle(
-            //     weekdayStyle:
-            //         AppTextStyle.paragraphM.copyWith(color: AppColors.black),
-            //     weekendStyle:
-            //         AppTextStyle.paragraphM.copyWith(color: AppColors.primary1),
-            //   ),
-            //   headerStyle: HeaderStyle(
-            //     titleCentered: true,
-            //     formatButtonVisible: false,
-            //     titleTextStyle: AppTextStyle.paragraphMBold.copyWith(
-            //       color: AppColors.black,
-            //     ),
-            //   ),
-            // )
-            const Gap(8),
+            const Gap(16),
+            FutureBuilder(
+              future: userPrefs.getUser(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.data!.roles!.contains('staff')) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Lokasi Terakhir',
+                          style: AppTextStyle.paragraphLBold),
+                      const Gap(8),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 172,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(16),
+                          child: FlutterMap(
+                            options: const MapOptions(
+                              initialCenter: LatLng(-6.8865473, 107.6120931),
+                              minZoom: 9,
+                            ),
+                            children: [
+                              TileLayer(
+                                urlTemplate:
+                                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                userAgentPackageName: 'com.example.app',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+            const Gap(16),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
